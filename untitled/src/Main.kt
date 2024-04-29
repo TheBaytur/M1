3cc7151f0942d77738d7d2f94cc964fc7468c73c
TheBaytur <bayturcodding@gmail.com>
2024-04-30 01:14:32 +0800

    mac1

--- untitled/src/Main.kt
+++ untitled/src/Main.kt
@@ -1,5 +1,5 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
+fun main() { //M1
     println("Hello World!")
 }
